c25a94a706e91a3d0afdeafac361535d5ac488cd
Joshua Paul <paul.josh62@gmail.com>
2022-11-14 11:50:34 -0600

    done

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const MY_API_KEY = `XpWnP8GYtSRPzRq790cGMICxwfNZfFUN`
+
+
+let buttonSearch = document.querySelector('#submitSearch');
+let inputSearch = document.querySelector('#searchWord');
+let imgSearch = document.querySelector('#imageContainer > img');
+let feedbackPara = document.querySelector('#feedbackPara');
+
+console.log(buttonSearch, inputSearch, imgSearch, feedbackPara);
+
+
+//eventlitener-click-->added-->Search Button
+buttonSearch.addEventListener('click', ()=>{
+  let searchTerm = inputSearch.value; 
+  console.log(inputSearch.value);
+  fetch(`https://api.giphy.com/v1/gifs/translate?apikey=${MY_API_KEY}&s=${searchTerm}`)
+  .then((resolvedValue) => {
+    return resolvedValue.json()
+  })
+  .then((res) => {
+    //console.log(res);
+    imgSearch.src = res.data.images.original.url;
+    inputSearch.value = "";
+  })
+  .catch((failstate) => {
+    console.log(failstate);
+    feedbackPara.textContent = failstate.message;
+  })
+});
+
+
+
+
+
